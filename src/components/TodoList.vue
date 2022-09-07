481040eaf25a327e67b1cd8c5e7b03c911ad6e4b
Duong Tan <thanhtantt1998@gmail.com>
2022-09-07 13:36:24 +0700

    add & delete task

---
+++ src/components/TodoList.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="container">
+    <h2 class="text-center mt-4">Các công việc cần làm</h2>
+
+    <!-- noi nhap input -->
+    <div class="d-flex mt-3">
+      <input
+        type="text"
+        v-model="task"
+        placeholder="Nhập công việc cần làm"
+        class="form-control"
+      />
+      <button @click="submitTask" class="btn btn-success rounded-0">
+        Submit
+      </button>
+    </div>
+
+    <!-- Bang cong viec can lam -->
+    <table class="table table-bordered mt-3">
+      <thead>
+        <tr>
+          <th scope="col">Task</th>
+          <th scope="col">Status</th>
+          <th scope="col">Edit</th>
+          <th scope="col">Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(task, index) in tasks" :key="index">
+          <td>{{ task.name }}</td>
+          <td>{{ task.status }}</td>
+          <td>
+            <div class="d-flex justify-content-center">
+              <span class="fa fa-pen"></span>
+            </div>
+          </td>
+          <td>
+            <div
+              @click="deleteTask(index)"
+              class="d-flex justify-content-center"
+            >
+              <span class="fa fa-trash"></span>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoList",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      task: "",
+      tasks: [
+        {
+          name: "Day som 6h sang",
+          status: "to do",
+        },
+        {
+          name: "Tap gym vao 5h chieu",
+          status: "in process",
+        },
+      ],
+    };
+  },
+
+  methods: {
+    submitTask() {
+      if (this.task.length === 0) return;
+
+      this.tasks.push({
+        name: this.task,
+        status: "to do",
+      });
+
+      this.task = "";
+    },
+
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
